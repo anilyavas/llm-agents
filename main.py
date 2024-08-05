fbdb1cc1b9647b2200aba42a2224438a88030635
anilyavas <anil.yavas99@gmail.com>
2024-08-05 19:44:14 +0300

    agent

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
 from dotenv import load_dotenv
+from langchain.agents import AgentExecutor, OpenAIFunctionsAgent
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    MessagesPlaceholder,
+)
+
+from tools.sql import run_query_tool
 
 load_dotenv()
 
-print("hello")
+chat = ChatOpenAI()
+
+prompt = ChatPromptTemplate(
+    messages=[
+        HumanMessagePromptTemplate.from_template("{input}"),
+        MessagesPlaceholder(variable_name="agent_scratchpad"),
+    ]
+)
+
+tools = [run_query_tool]
+
+agent = OpenAIFunctionsAgent(
+    llm=chat,
+    prompt=prompt,
+    tools=tools,
+)
+
+agent_executer = AgentExecutor(
+    agent=agent,
+    verbose=True,
+    tools=tools,
+)
+
+agent_executer("How many users are there in the database?")
