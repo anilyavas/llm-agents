22d6b726948f43811c7f583ab24f5ff20aa8e16f
anilyavas <anil.yavas99@gmail.com>
2024-08-08 15:05:10 +0300

    optimization

--- main.py
+++ main.py
@@ -6,21 +6,32 @@ from langchain.prompts import (
     HumanMessagePromptTemplate,
     MessagesPlaceholder,
 )
+from langchain.schema import SystemMessage
 
-from tools.sql import run_query_tool
+from tools.sql import describe_tables_tool, list_tables, run_query_tool
 
 load_dotenv()
 
 chat = ChatOpenAI()
 
+tables = list_tables()
+
 prompt = ChatPromptTemplate(
     messages=[
+        SystemMessage(
+            content=(
+                "You are an AI that has access to a SQLite database.\n"
+                f"The database has tables of: {tables}\n"
+                "Do not make any assumptions about what tables exist"
+                "or what colums exist. Instead, use the 'describe_tables' function"
+            )
+        ),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad"),
     ]
 )
 
-tools = [run_query_tool]
+tools = [run_query_tool, describe_tables_tool]
 
 agent = OpenAIFunctionsAgent(
     llm=chat,
